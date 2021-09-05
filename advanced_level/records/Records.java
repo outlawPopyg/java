be25f6771efa15acc2223c596dc3c423ff753817
Popyg <tyl9ray@yandex.ru>
2021-09-05 19:19:01 +0300

    +records

---
+++ advanced_level/records/Records.java
@@ -0,0 +1,16 @@
+public class Records {
+	public static void main(String[] args) {
+		Person tom = new Person("Tom", 23);
+		System.out.println(tom.toString());
+		System.out.println(tom.name());     // Tom
+        System.out.println(tom.age());      // 36
+        System.out.println(tom.hashCode());
+         
+        Person bob = new Person("Bob", 21);
+        Person tomas = new Person("Tom", 23);
+        System.out.println(tom.equals(bob));    // false
+        System.out.println(tom.equals(tomas));  // true
+	}
+}
+
+record Person(String name, int age) { }
